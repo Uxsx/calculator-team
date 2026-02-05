fdc7277f3fe0810d49c2edce64cb27f8df8cc51e
Uxsx <snowlevelup@gmail.com>
2026-02-06 00:57:22 +0700

    edit main

--- source/Main.java
+++ source/Main.java
@@ -1,5 +1,37 @@
 package source;
+import java.util.Scanner;
 
-public class Main {
-    
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Calculator Program!");
+        new Display();
+
+        System.out.print("Input Number: ");
+        double initialNumber = scanner.nextDouble();
+
+        Calculator cal = new Calculator(initialNumber);
+
+        while (true) {
+            System.out.print("Input Operators: ");
+            String operation = scanner.next();
+
+            if (operation.equals("=")) {
+                System.out.println("-------------------------");
+                System.out.println("Final Result: " + cal.getResult());
+                break;
+            }
+
+            System.out.print("Input Next Number: ");
+            double nextNumber = scanner.nextDouble();
+
+            cal.applyOperation(operation, nextNumber);
+
+            new Display();
+            System.out.println("Current Result: " + cal.getResult());
+        }
+
+        scanner.close();
+    }
 }
